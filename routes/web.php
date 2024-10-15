52749404dae3d94f33f676b619ee88cd710b4f3b
Tung-NK <tungnguyenkhac222@gmail.com>
2024-10-15 20:39:21 +0700

    login logout admin

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,26 @@
 <?php
 
+use App\Http\Controllers\Admin\AccountController;
+use App\Http\Controllers\Admin\AuthenController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('users.home');
 }); // DEMO
 
-Route::get('admin', function () {
-    return view('admin.account.list-account');
-}); // DEMO
-
 
 // Làm từ đây nhé
 
-Route::prefix('admin') -> middleware('...')->group(function (){
-    // Route::get('/', [abc::class , 'fromLogin'])->name('fromLogin')->withoutMiddleware('checkAdmin'); 
-    //withoutMiddleware('....')-> bỏ qua kiểm tra của middleware
+Route::prefix('admin')->middleware('checkAdmin')->group(function () {
+    Route::get('/', [AuthenController::class, 'formLogin'])->name('formLogin')->withoutMiddleware('checkAdmin');
+    Route::post('postLogin', [AuthenController::class, 'postLogin'])->name('postLogin')->withoutMiddleware('checkAdmin');
+    Route::get('logout', [AuthenController::class, 'logout'])->name('logout');
 
 
-    Route::prefix('movie') -> name('movie.') -> group(function (){
-        // GET, POST, PUTH , PATCH, DELETE
+    Route::prefix('movie')->name('account')->group(function () {
+        Route::get('/', [AccountController::class, 'index'])->name('list');
     });
-
 });
 
 
-Route::prefix('/') -> middleware('...')->group(function (){
-  
-});
+Route::prefix('/')->middleware('...')->group(function () {});
