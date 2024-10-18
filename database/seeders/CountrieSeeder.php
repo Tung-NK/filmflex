235909ed889bd8d7ecce51f669542d2eea5a7984
Tung-NK <tungnguyenkhac222@gmail.com>
2024-10-18 20:54:40 +0700

    reset password

--- database/seeders/CountrieSeeder.php
+++ database/seeders/CountrieSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Countrie;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\admin\Countrie;
 class CountrieSeeder extends Seeder
 {
     /**
